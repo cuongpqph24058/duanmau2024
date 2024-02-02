000371b0251002040db1c0c14160379a9bbb89d6
cuongpham <114381515+cuongpqph24058@users.noreply.github.com>
2024-02-02 15:55:29 +0700

    first commit

--- model/sanpham.php
+++ model/sanpham.php
@@ -10,6 +10,18 @@ function delete_sanpham($id){
     pdo_execute($sql);
 }
 
+function loadall_sanpham_top10(){
+    $sql = "select * from sanpham where 1 order by luotxem desc limit 0,10"; 
+    $listsanpham = pdo_query($sql);
+    return $listsanpham;
+}
+
+function loadall_sanpham_home(){
+    $sql = "select * from sanpham where 1 order by id desc limit 0,9"; 
+    $listsanpham = pdo_query($sql);
+    return $listsanpham;
+}
+
 function loadall_sanpham($kyw="", $iddm=0){
     $sql = "select * from sanpham where 1"; 
     if($kyw!=""){
@@ -23,11 +35,27 @@ function loadall_sanpham($kyw="", $iddm=0){
     $listsanpham = pdo_query($sql);
     return $listsanpham;
 }
-
+function loado_ten_dm($iddm){
+    if($iddm>0){
+    $sql="select * from danhmuc where id=".$iddm;
+    $dm=pdo_query_one($sql);
+    extract($dm);
+    return $name;
+    }else{
+        return "";
+    }
+}
 function loadone_sanpham($id){
     $sql="select * from sanpham where id=".$id;
-    $dm=pdo_query_one($sql);
-    return $dm;
+    $sp=pdo_query_one($sql);
+    return $sp;
+    }
+
+    
+function load_sanpham_cungloai($id,$iddm){
+    $sql="select * from sanpham where iddm=".$iddm." AND id <>".$id;
+    $listsanpham = pdo_query($sql);
+    return $listsanpham;
     }
 
 function update_sanpham($id,$iddm,$tensp,$giasp,$mota,$hinh){
---
+++ view/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dự án mẫu 2024</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="view/css/style.css">
+</head>
+
+<body>
+    <div class="boxcenter">
+        <div class="row mb header">
+            <h1>Siêu thị trực tuyến</h1>
+        </div>
+
+        <div class="row mb menu">
+            <ul>
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="index.php?act=gioithieu">Giới thiệu</a></li>
+                <li><a href="index.php?act=lienhe">Liên hệ</a></li>
+                <li><a href="index.php?act=gopy">Góp ý</a></li>
+                <li><a href="index.php?act=hoidap">Hỏi đáp</a></li>
+            </ul>
+        </div>
